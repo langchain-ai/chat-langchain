5f9b59a38e04d331f40202a8513be9ab893ebbf3
TOM_DARMON <darmontom14@gmail.com>
2023-03-05 17:41:46 +0100

    scrap blank website

--- ingest.py
+++ ingest.py
@@ -5,23 +5,27 @@ from langchain.document_loaders import ReadTheDocsLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    loader = ReadTheDocsLoader("aide.blank.app/")
     raw_documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
     )
-    documents = text_splitter.split_documents(raw_documents)
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_documents(documents, embeddings)
 
-    # Save vectorstore
-    with open("vectorstore.pkl", "wb") as f:
-        pickle.dump(vectorstore, f)
+    # documents = text_splitter.split_documents(raw_documents)
+    # embeddings = OpenAIEmbeddings()
+    # vectorstore = FAISS.from_documents(documents, embeddings)
+
+    # # Save vectorstore
+    # with open("vectorstore.pkl", "wb") as f:
+    #     pickle.dump(vectorstore, f)
 
 
 if __name__ == "__main__":
