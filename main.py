f54daf49ec374367d8fd645eed914b2c60c6a0c2
mrmattwright <matt.wright@montoux.com>
2023-04-02 17:19:59 +1200

    updated deployment

--- main.py
+++ main.py
@@ -101,4 +101,4 @@ def format_data_sources(source_docs) -> List[DataSource]:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
