0f20a536b7a574a640eda2be1e2a511bf6b8f5f2
Jon Bennett <jon.bennett@futurenet.com>
2023-04-29 13:07:49 +0100

    fix: Switch to ConversationalRetrievalChain to get async support

--- query_data.py
+++ query_data.py
@@ -1,7 +1,7 @@
 """Create a ChatVectorDBChain for question/answering."""
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
-from langchain.chains import ChatVectorDBChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
                                                      QA_PROMPT)
 from langchain.chains.llm import LLMChain
@@ -45,10 +45,11 @@ def get_chain(
         streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
     )
 
-    qa = ChatVectorDBChain(
-        vectorstore=vectorstore,
+    qa = ConversationalRetrievalChain(
+        retriever=vectorstore.as_retriever(),
         combine_docs_chain=doc_chain,
         question_generator=question_generator,
         callback_manager=manager,
     )
+    
     return qa
