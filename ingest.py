cfa6aef6d8aca3d706430304e370a4d7ec3813b1
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-08-09 13:54:13 -0700

    switch to weaviate, recursiveurlloader, openaiembeddings

--- ingest.py
+++ ingest.py
@@ -1,42 +1,53 @@
 """Load html from files, clean up, split, ingest into Weaviate."""
-import pickle
+from bs4 import BeautifulSoup as Soup
+import weaviate
+import os
 
-from langchain.document_loaders import ReadTheDocsLoader
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
+from langchain.embeddings import OpenAIEmbeddings, CohereEmbeddings
+from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
-from langchain.document_loaders import RecursiveUrlLoader
-import numpy as np
+from langchain.vectorstores import Weaviate
+
+WEAVIATE_URL=os.environ["WEAVIATE_URL"]
+WEAVIATE_API_KEY=os.environ["WEAVIATE_API_KEY"]
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("api.python.langchain.com/en/latest", features="lxml")
-    loader2 = ReadTheDocsLoader("python.langchain.com", features="lxml")
-    # loader2 = RecursiveUrlLoader(url="https://python.langchain.com/docs/get_started/introduction", max_depth=2, extractor=lambda x: Soup(x, "lxml").text)
 
-    raw_documents = loader.load()
-    print(len(raw_documents))
-    raw_documents += loader2.load()
-    print(len(raw_documents))
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-    )
-    documents = text_splitter.split_documents(raw_documents)
-    print(len(documents))
-    embeddings = OpenAIEmbeddings(chunk_size=100)
-    print(embeddings)
-    vectorstore = FAISS.from_documents(documents, embeddings)
+    urls = [
+        "https://api.python.langchain.com/en/latest/api_reference.html#module-langchain",
+        "https://python.langchain.com/docs/get_started", 
+        "https://python.langchain.com/docs/modules", 
+        "https://python.langchain.com/docs/guides",
+        "https://python.langchain.com/docs/ecosystem",
+        "https://python.langchain.com/docs/additional_resources",
+    ]
+
+    documents = []
+    for url in urls:
+        loader = RecursiveUrlLoader(url=url, max_depth=1 if url == urls[0] else 3, extractor=lambda x: Soup(x, "lxml").text, prevent_outside=False)
+        documents += loader.load()
     
-    # Ensure all embeddings have the same length
-    max_length = max(len(e) for e in embeddings)
-    padded_embeddings = np.array([e + tuple([0]*(max_length-len(e))) for e in embeddings])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+    print('before splitting', len(documents))
+    docs = text_splitter.split_documents(documents)
+    print('after splitting', len(docs))
+
+    embeddings = OpenAIEmbeddings(chunk_size=200) # rate limit
+
+    client = weaviate.Client(url=WEAVIATE_URL, auth_client_secret=weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY))
+    client.schema.delete_all()
+    print(client.schema.get()) # should be empty
+
+    batch_size = 100 # to handle batch size limit 
+    for i in range(0, len(docs), batch_size):
+        batch = docs[i:i+batch_size]
+        Weaviate.from_documents(batch, embeddings, client=client, by_text=False, index_name="LangChain_idx")
+
+    print("LangChain now has this many vectors", client.query.aggregate("LangChain_idx").with_meta_count().do())
     
-    # Save vectorstore
-    with open("vectorstore.pkl", "wb") as f:
-        pickle.dump(vectorstore, f)
-
-
 if __name__ == "__main__":
     ingest_docs()
     
