54f0f6848c60672751568b57b59c5a4a170d144d
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2023-08-25 17:28:04 -0700

    delete archive and parentsplit retriever

--- archive/archive_recent/ingest.sh
+++
@@ -1,7 +0,0 @@
-# Bash script to ingest data
-# This involves scraping the data from the web and then cleaning up and putting in Weaviate.
-# Error if any command fails
-set -e
-wget -r -A.html https://api.python.langchain.com/en/latest
-wget -r -A.html https://python.langchain.com/docs/get_started/introduction
-python3 ingest.py
--- ingest.py
+++ ingest.py
@@ -44,24 +44,6 @@ def ingest_repo():
     texts = python_splitter.split_documents(documents_repo)
     return texts
 
-def key_encoder(key: int) -> str:
-    return json.dumps(key)
-
-def value_serializer(value: float) -> str:
-    if isinstance(value, Document):
-        value = {
-            'page_content': value.page_content,
-            'metadata': value.metadata,
-        }
-    return json.dumps(value)
-
-def value_deserializer(serialized_value: str) -> Document:
-    value = json.loads(serialized_value)
-    if 'page_content' in value and 'metadata' in value:
-        return Document(page_content=value['page_content'], metadata=value['metadata'])
-    else:
-        return value
-    
 def ingest_docs():
     """Get documents from web pages."""
 
@@ -79,7 +61,7 @@ def ingest_docs():
 
     documents = []
     for url in urls:
-        loader = RecursiveUrlLoader(url=url, max_depth=2 if url == urls[0] else 10, extractor=lambda x: Soup(x, "lxml").text, prevent_outside=True)
+        loader = RecursiveUrlLoader(url=url, max_depth=2 if url == urls[0] else 8, extractor=lambda x: Soup(x, "lxml").text, prevent_outside=True)
         temp_docs = loader.load()
         temp_docs = [doc for i, doc in enumerate(temp_docs) if doc not in temp_docs[:i]]        
         documents += temp_docs
@@ -90,60 +72,33 @@ def ingest_docs():
     html2text = Html2TextTransformer()
     docs_transformed = html2text.transform_documents(documents)
     
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+    print('before splitting', len(docs_transformed))
+    docs_transformed = text_splitter.split_documents(docs_transformed)
+    print('after splitting', len(docs_transformed))
+    
     repo_docs = ingest_repo()
     docs_transformed += repo_docs
-    
-    print("Transformed", len(docs_transformed), "documents in total")
-    
+        
     # OPTION TO PICKLE
-    # import pickle
-    # with open('docs_transformed.pkl', 'wb') as f:
-    #     pickle.dump(docs_transformed, f)
+    import pickle
+    with open('docs_transformed.pkl', 'wb') as f:
+        pickle.dump(docs_transformed, f)
+        
     # with open('docs_transformed.pkl', 'rb') as f:
     #     docs_transformed = pickle.load(f)
     
     client = weaviate.Client(url=WEAVIATE_URL, auth_client_secret=weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY))
-    child_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
-    parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
-    client.schema.delete_class("LangChain_parents_idx") # delete the class if it already exists
-    
-    vectorstore = Weaviate(
-        embedding=OpenAIEmbeddings(chunk_size=200),
-        client=client,
-        index_name="LangChain_parents_idx",
-        by_text=False,
-        text_key="text",
-        attributes=["doc_id"],
-    )
+    client.schema.delete_class("LangChain_newest_idx") # delete the class if it already exists
 
-    redis_client = get_client('redis://localhost:6379')
-    abstract_store = RedisStore(client=redis_client)
+    embeddings = OpenAIEmbeddings(chunk_size=200) # rate limit
 
-    # Create an instance of the encoder-backed store
-    store = EncoderBackedStore(
-        store=abstract_store,
-        key_encoder=key_encoder,
-        value_serializer=value_serializer,
-        value_deserializer=value_deserializer
-    )
+    batch_size = 100 # to handle batch size limit 
+    for i in range(0, len(docs_transformed), batch_size):
+        batch = docs_transformed[i:i+batch_size]
+        Weaviate.from_documents(batch, embeddings, client=client, by_text=False, index_name="LangChain_newest_idx")
 
-    retriever = ParentDocumentRetriever(
-        vectorstore=vectorstore, 
-        docstore=store, 
-        child_splitter=child_splitter,
-        parent_splitter=parent_splitter
-    )
-            
-    # client.schema.delete_all()
-    # print(client.schema.get()) # should be empty
-        
-    batch = 100
-    for i in range(0, len(docs_transformed), batch):
-        chunk = docs_transformed[i:i+batch]
-        retriever.add_documents(chunk, ids=None)
-
-    print("LangChain now has this many vectors", client.query.aggregate("LangChain_parents_idx").with_meta_count().do())
+    print("LangChain now has this many vectors", client.query.aggregate("LangChain_newest_idx").with_meta_count().do())
     
 if __name__ == "__main__":
     ingest_docs()
-    
