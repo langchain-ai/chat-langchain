82f3f94110aed7a5379aaea2a6ccb4ec2069ba72
Bagatur <baskaryan@gmail.com>
2023-09-05 16:33:16 -0700

    undo rename

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-WEAVIATE_REPO_INDEX_NAME = "LangChain_agent_repo_indexed"
-WEAVIATE_DOCS_INDEX_NAME = "LangChain_agent_docs_indexed"
-WEAVIATE_SOURCES_INDEX_NAME = "LangChain_agent_sources_indexed"
+WEAVIATE_REPO_INDEX_NAME = "LangChain_agent_repo"
+WEAVIATE_DOCS_INDEX_NAME = "LangChain_agent_docs"
+WEAVIATE_SOURCES_INDEX_NAME = "LangChain_agent_sources"
