aad11a8bfc661dc7e368d3f93f9c7e86149f4b18
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-26 15:15:42 -0700

    update

--- _scripts/evaluate_chat_langchain.py
+++ _scripts/evaluate_chat_langchain.py
@@ -10,6 +10,7 @@ from langchain.smith import RunEvalConfig
 from langsmith import Client, RunEvaluator
 from langsmith.evaluation.evaluator import EvaluationResult
 from langsmith.schemas import Example, Run
+
 # Ugly. Requires PYTHONATH=$(PWD) to run
 from main import create_chain, get_retriever
 
@@ -22,31 +23,6 @@ _MODEL_MAP = {
     "openai": "gpt-3.5-turbo-16k",
     "anthropic": "claude-2",
 }
-
-
-class CustomHallucinationEvaluator(RunEvaluator):
-    @staticmethod
-    def _get_llm_runs(run: Run) -> Run:
-        runs = []
-        for child in run.child_runs or []:
-            if run.run_type == "llm":
-                runs.append(child)
-            else:
-                runs.extend(CustomHallucinationEvaluator._get_llm_runs(child))
-
-    def evaluate_run(
-        self, run: Run, example: Example | None = None
-    ) -> EvaluationResult:
-        llm_runs = self._get_llm_runs(run)
-        if not llm_runs:
-            return EvaluationResult(key="hallucination", comment="No LLM runs found")
-        if len(llm_runs) > 0:
-            return EvaluationResult(
-                key="hallucination", comment="Too many LLM runs found"
-            )
-        llm_run = llm_runs[0]
-        messages = llm_run.inputs["messages"]
-        langchain_load(json.dumps(messages))
 
 
 if __name__ == "__main__":
@@ -73,8 +49,11 @@ if __name__ == "__main__":
             "question": lambda x: x["question"],
             "chat_history": (lambda x: x.get("chat_history", [])),
         } | chain
-    
-    eval_config = RunEvalConfig(evaluators=["qa"], prediction_key="output")
+
+    eval_config = RunEvalConfig(
+        evaluators=["qa"],
+        prediction_key="output",
+    )
     results = client.run_on_dataset(
         dataset_name=args.dataset_name,
         llm_or_chain_factory=construct_eval_chain,
