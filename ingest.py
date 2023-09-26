38f903528295fafec6f305e14617c03ca6aa41c4
William FH <13333726+hinthornw@users.noreply.github.com>
2023-09-26 14:02:09 -0700

    Add Run Names (#144)

--- ingest.py
+++ ingest.py
@@ -2,10 +2,12 @@
 import logging
 import os
 import re
+from parser import langchain_docs_extractor
 
 import weaviate
 from bs4 import BeautifulSoup, SoupStrainer
-from langchain.document_loaders import SitemapLoader, RecursiveUrlLoader
+from langchain.document_loaders import RecursiveUrlLoader, SitemapLoader
+from langchain.document_loaders.recursive_url_loader import RecursiveUrlLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import SQLRecordManager, index
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -13,7 +15,6 @@ from langchain.utils.html import PREFIXES_TO_IGNORE_REGEX, SUFFIXES_TO_IGNORE_RE
 from langchain.vectorstores import Weaviate
 
 from constants import WEAVIATE_DOCS_INDEX_NAME
-from parser import langchain_docs_extractor
 
 logger = logging.getLogger(__name__)
 
