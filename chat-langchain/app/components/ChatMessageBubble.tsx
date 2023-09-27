9644bdf28ec1674f319ed5f28300a495b531eacc
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-26 20:10:52 -0700

    .''

--- chat-langchain/app/components/ChatMessageBubble.tsx
+++ chat-langchain/app/components/ChatMessageBubble.tsx
@@ -38,7 +38,6 @@ export function ChatMessageBubble(props: {
   const [isLoading, setIsLoading] = useState(false);
   const [feedback, setFeedback] = useState<Feedback | null>(null);
   const [comment, setComment] = useState("");
-  const [showCommentForm, setShowCommentForm] = useState(false);
   const [feedbackColor, setFeedbackColor] = useState("");
 
   const cumulativeOffset = function (element: HTMLElement | null) {
@@ -55,12 +54,6 @@ export function ChatMessageBubble(props: {
       left: left,
     };
   };
-
-  async function handleCommentSubmission(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const score = typeof feedback?.score === "number" ? feedback.score : 0;
-    await sendFeedback(score, "user_score");
-  }
 
   const sendFeedback = async (score: number, key: string) => {
     let run_id = runId;
@@ -96,9 +89,6 @@ export function ChatMessageBubble(props: {
         score == 1 ? animateButton("upButton") : animateButton("downButton");
         if (comment) {
           setComment("");
-          setShowCommentForm(false);
-        } else {
-          setShowCommentForm(true);
         }
       }
     } catch (e: any) {
@@ -276,50 +266,6 @@ export function ChatMessageBubble(props: {
             >
               👎
             </button>
-            <div
-              className={`${
-                // Synchronous feedback currently flakey. Need to wait for
-                // updates to the queue.
-                false && feedback && showCommentForm ? "" : "hidden"
-              } min-w-[480px]`}
-            >
-              <form onSubmit={handleCommentSubmission} className="relative">
-                <input
-                  className="mr-8 p-4 rounded w-full border mt-2"
-                  value={comment}
-                  placeholder={
-                    feedback?.score === 1
-                      ? "Anything else you'd like to add about this response?"
-                      : "What would the correct or preferred response have been?"
-                  }
-                  onChange={(e) => setComment(e.target.value)}
-                />
-                <div
-                  role="status"
-                  className={`${
-                    isLoading ? "" : "hidden"
-                  } flex justify-center absolute top-[24px] right-[16px]`}
-                >
-                  <svg
-                    aria-hidden="true"
-                    className="w-6 h-6 text-slate-200 animate-spin dark:text-slate-200 fill-sky-800"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
-                  </svg>
-                  <span className="sr-only">Loading...</span>
-                </div>
-              </form>
-            </div>
           </div>
         )}
 
