97d33b0c96ab54247fa1585f4c56b7cd8aea895d
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2023-09-26 19:28:32 -0700

    .

--- chat-langchain/app/components/ChatMessageBubble.tsx
+++ chat-langchain/app/components/ChatMessageBubble.tsx
@@ -1,10 +1,11 @@
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { emojisplosion } from "emojisplosion";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { SourceBubble, Source } from "./SourceBubble";
 import { Flex, Box, Heading, HStack, VStack, Divider } from "@chakra-ui/react";
 import { InlineCitation } from "./InlineCitation";
+import { v4 as uuidv4 } from "uuid";
 
 export type Message = {
   id: string;
@@ -16,17 +17,28 @@ export type Message = {
   name?: string;
   function_call?: { name: string };
 };
+export interface Feedback {
+  feedback_id: string;
+  run_id: string;
+  key: string;
+  score: number;
+  comment?: string;
+}
 
 export function ChatMessageBubble(props: {
   message: Message;
   aiEmoji?: string;
-  feedback?: number;
-  sendFeedback: (feedback?: number, run_id?: string) => void;
   isMostRecent: boolean;
   messageCompleted: boolean;
+  apiBaseUrl: string;
 }) {
-  const isUser = props.message.role === "user";
+  const { role, content, runId } = props.message;
+  const isUser = role === "user";
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [feedback, setFeedback] = useState<Feedback | null>(null);
+  const [comment, setComment] = useState("");
+  const [showCommentForm, setShowCommentForm] = useState(false);
   const [feedbackColor, setFeedbackColor] = useState("");
 
   const cumulativeOffset = function (element: HTMLElement | null) {
@@ -44,6 +56,57 @@ export function ChatMessageBubble(props: {
     };
   };
 
+  async function handleCommentSubmission(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const score = typeof feedback?.score === "number" ? feedback.score : 0;
+    await sendFeedback(score, "user_score");
+  }
+
+  const sendFeedback = async (score: number, key: string) => {
+    let run_id = runId;
+    console.log("Sending feedback", run_id, score, key, comment);
+    if (run_id === undefined) {
+      return;
+    }
+    if (isLoading) {
+      return;
+    }
+    setIsLoading(true);
+    console.log("Still loading/sending feedback", run_id, score, key, comment);
+    let apiBaseUrl = props.apiBaseUrl;
+    let feedback_id = feedback?.feedback_id ?? uuidv4();
+    try {
+      const response = await fetch(apiBaseUrl + "/feedback", {
+        method: feedback?.feedback_id ? "PATCH" : "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          score,
+          run_id,
+          key,
+          feedback_id,
+          comment,
+        }),
+      });
+      console.log("Response", response);
+      const data = await response.json();
+      if (data.code === 200) {
+        setFeedback({ run_id, score, key, feedback_id });
+        score == 1 ? animateButton("upButton") : animateButton("downButton");
+        if (comment) {
+          setComment("");
+          setShowCommentForm(false);
+        } else {
+          setShowCommentForm(true);
+        }
+      }
+    } catch (e: any) {
+      console.error("Error:", e);
+      toast.error(e.message);
+    }
+    setIsLoading(false);
+  };
 
   const sources = props.message.sources ?? [];
 
@@ -52,8 +115,7 @@ export function ChatMessageBubble(props: {
   const [highlighedSourceLinkStates, setHighlightedSourceLinkStates] = useState(
     sources.map(() => false)
   );
-  let standaloneMessage = props.message.content;
-  const matches = Array.from(standaloneMessage.matchAll(/\[(\d+)\]/g));
+  const matches = Array.from(content.matchAll(/\[(\d+)\]/g));
   const answerElements = [];
 
   let previousSliceIndex = 0;
@@ -64,7 +126,7 @@ export function ChatMessageBubble(props: {
         <span
           key={`content:${previousSliceIndex}`}
           dangerouslySetInnerHTML={{
-            __html: standaloneMessage.slice(previousSliceIndex, match.index),
+            __html: content.slice(previousSliceIndex, match.index),
           }}
         ></span>
       );
@@ -91,7 +153,7 @@ export function ChatMessageBubble(props: {
     <span
       key={`content:${previousSliceIndex}`}
       dangerouslySetInnerHTML={{
-        __html: standaloneMessage.slice(previousSliceIndex),
+        __html: content.slice(previousSliceIndex),
       }}
     ></span>
   );
@@ -167,7 +229,7 @@ export function ChatMessageBubble(props: {
       )}
       {isUser ? (
         <Heading size={"lg"} fontWeight={"medium"} color={"white"}>
-          {standaloneMessage}
+          {content}
         </Heading>
       ) : (
         <div className="whitespace-pre-wrap" style={{ color: "white" }}>
@@ -180,13 +242,13 @@ export function ChatMessageBubble(props: {
           <div className="relative flex space-x-1 items-start justify-start">
             <button
               className={`text-sm rounded ${
-                props.feedback === undefined ? "hover:bg-green-200" : ""
+                feedback === null ? "hover:bg-green-200" : ""
               }`}
               id="upButton"
               type="button"
               onClick={() => {
-                if (props.feedback === undefined) {
-                  props.sendFeedback(1, props.message.runId);
+                if (feedback === null && props.message.runId) {
+                  sendFeedback(1, "user_score");
                   animateButton("upButton");
                   setFeedbackColor("border-4 border-green-300");
                 } else {
@@ -198,13 +260,13 @@ export function ChatMessageBubble(props: {
             </button>
             <button
               className={`text-sm rounded ${
-                props.feedback === undefined ? "hover:bg-red-200" : ""
+                feedback === null ? "hover:bg-red-200" : ""
               }`}
               id="downButton"
               type="button"
               onClick={() => {
-                if (props.feedback === undefined) {
-                  props.sendFeedback(0, props.message.runId);
+                if (feedback === null && props.message.runId) {
+                  sendFeedback(0, "user_score");
                   animateButton("downButton");
                   setFeedbackColor("border-4 border-red-300");
                 } else {
@@ -214,6 +276,50 @@ export function ChatMessageBubble(props: {
             >
               👎
             </button>
+            <div
+              className={`${
+                // Synchronous feedback currently flakey. Need to wait for
+                // updates to the queue.
+                false && feedback && showCommentForm ? "" : "hidden"
+              } min-w-[480px]`}
+            >
+              <form onSubmit={handleCommentSubmission} className="relative">
+                <input
+                  className="mr-8 p-4 rounded w-full border mt-2"
+                  value={comment}
+                  placeholder={
+                    feedback?.score === 1
+                      ? "Anything else you'd like to add about this response?"
+                      : "What would the correct or preferred response have been?"
+                  }
+                  onChange={(e) => setComment(e.target.value)}
+                />
+                <div
+                  role="status"
+                  className={`${
+                    isLoading ? "" : "hidden"
+                  } flex justify-center absolute top-[24px] right-[16px]`}
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-6 h-6 text-slate-200 animate-spin dark:text-slate-200 fill-sky-800"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </form>
+            </div>
           </div>
         )}
 
--- main.py
+++ main.py
@@ -213,16 +213,33 @@ async def chat_endpoint(request: ChatRequest):
 @app.post("/feedback")
 async def send_feedback(request: Request):
     data = await request.json()
-    score = data.get("score")
-    run_id = data.get("run_id")  # TODO：prevent duplicate feedback
+    run_id = data.get("run_id")
     if run_id is None:
         return {
             "result": "No LangSmith run ID provided",
             "code": 400,
         }
-
-    client.create_feedback(run_id, "user_score", score=score)
+    key = data.get("key", "user_score")
+    vals = {**data, "key": key}
+    client.create_feedback(**vals)
     return {"result": "posted feedback successfully", "code": 200}
+
+
+@app.patch("/feedback")
+async def update_feedback(request: Request):
+    data = await request.json()
+    feedback_id = data.get("feedback_id")
+    if feedback_id is None:
+        return {
+            "result": "No feedback ID provided",
+            "code": 400,
+        }
+    client.update_feedback(
+        feedback_id,
+        score=data.get("score"),
+        comment=data.get("comment"),
+    )
+    return {"result": "patched feedback successfully", "code": 200}
 
 
 trace_url = None
