3de16f8318abce7e88dc1da5444d9fb067a46200
Jacob Lee <jacoblee93@gmail.com>
2023-10-06 22:48:18 -0700

    Switch to text/event-stream (#189)
    * Switch to text/event-stream
    
    * Format

--- chat-langchain/app/api/chat/route.ts
+++ chat-langchain/app/api/chat/route.ts
@@ -25,7 +25,12 @@ export const runtime = "edge";
 const RESPONSE_TEMPLATE = `You are an expert programmer and problem-solver, tasked to answer any question about Langchain. Using the provided context, answer the user's question to the best of your ability using the resources provided.
 Generate a comprehensive and informative answer (but no more than 80 words) for a given question based solely on the provided search results (URL and content). You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text. Cite search results using [\${{number}}] notation. Only cite the most relevant results that answer the question accurately. Place these citations at the end of the sentence or paragraph that reference them - do not put them all at the end. If different results refer to different entities within the same name, write separate answers for each entity.
 If there is nothing in the context relevant to the question at hand, just say "Hmm, I'm not sure." Don't try to make up an answer.
+
+You should use bullet points in your answer for readability. Put citations where they apply
+rather than putting them all at the end.
+
 Anything between the following \`context\`  html blocks is retrieved from a knowledge bank, not part of the conversation with the user.
+
 <context>
     {context}
 <context/>
@@ -196,12 +201,20 @@ export async function POST(req: NextRequest) {
     const clientStream = new ReadableStream({
       async start(controller) {
         for await (const chunk of stream) {
-          controller.enqueue(textEncoder.encode(JSON.stringify(chunk) + "\n"));
+          controller.enqueue(
+            textEncoder.encode(
+              "event: data\ndata: " + JSON.stringify(chunk) + "\n\n",
+            ),
+          );
         }
+        controller.enqueue(textEncoder.encode("event: end\n\n"));
+        controller.close();
       },
     });
 
-    return new Response(clientStream);
+    return new Response(clientStream, {
+      headers: { "Content-Type": "text/event-stream" },
+    });
   } catch (e: any) {
     console.log(e);
     return NextResponse.json({ error: e.message }, { status: 500 });
