40e19e662e91695f0af30d6444dd7a938d22069e
duob-ai <141818514+duob-ai@users.noreply.github.com>
2023-12-03 18:13:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,19 +35,9 @@ from crawler.i3_crawler import i3_crawler
 from crawler.trude_crawler import trude_crawler
 from datastore.factory import get_datastore
 from services.file import get_document_from_file
+from chains.rag_chain import create_answer_chain
 
 from constants import WEAVIATE_DOCS_INDEX_NAME
-
-datastore = None
-
-# Initialize Firebase Admin SDK
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-    'project_id': os.environ.get('GCP_PROJECT')
-})
-
-# Make the Firestore client available globally
-db = firestore.client()
 
 class CrawlerRequest(BaseModel):
     document_id: str
@@ -63,47 +53,34 @@ class APIKeyMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         return response
 
-RESPONSE_TEMPLATE = """\
-You are an expert Volksbank assistant, tasked with answering any question \
-about Volksbank internal knowladge that your colleagues might have. \
-You always answer in the same language as the question you get asked.
+class SendFeedbackBody(BaseModel):
+    run_id: UUID
+    key: str = "user_score"
 
-Generate a comprehensive and informative answer of 80 words or less for the \
-given question based solely on the provided search results (URL and content). You must \
-only use information from the provided search results. Use an unbiased and \
-journalistic tone. Combine search results together into a coherent answer. Do not \
-repeat text. If different results refer to different entities within the same name, write separate \
-answers for each entity.
+    score: Union[float, int, bool, None] = None
+    feedback_id: Optional[UUID] = None
+    comment: Optional[str] = None
 
-You should use bullet points in your answer for readability.
+class UpdateFeedbackBody(BaseModel):
+    feedback_id: UUID
+    score: Union[float, int, bool, None] = None
+    comment: Optional[str] = None
 
-If there is nothing in the context relevant to the question at hand, just say "Ich \
-habe leider keine Informationen darüber." Don't try to make up an answer.
+class GetTraceBody(BaseModel):
+    run_id: UUID
 
-Anything between the following `context`  html blocks is retrieved from a knowledge \
-data base, not part of the conversation with the user. 
-
-<context>
-    {context} 
-<context/>
-
-REMEMBER: If there is no relevant information within the context, just say "Ich \
-habe leider keine Informationen darüber." Don't try to make up an answer. \
-Anything between the preceding 'context' html blocks is retrieved from a knowledge bank, \ 
-not part of the conversation with the user.\
-"""
-
-REPHRASE_TEMPLATE = """\
-Given the following conversation and a follow up question, rephrase the follow up \
-question to be a standalone question.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone Question:"""
-
+datastore = None
 
 client = Client()
+
+# Initialize Firebase Admin SDK
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'project_id': os.environ.get('GCP_PROJECT')
+})
+
+# Make the Firestore client available globally
+db = firestore.client()
 
 app = FastAPI()
 app.add_middleware(APIKeyMiddleware)
@@ -116,145 +93,9 @@ app.add_middleware(
     expose_headers=["*"],
 )
 
-
-WEAVIATE_URL = os.environ["WEAVIATE_URL"]
-WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
-
-
-class ChatRequest(BaseModel):
-    question: str
-    chat_history: Optional[List[Dict[str, str]]]
-
-
-def get_embeddings_model() -> Embeddings:
-    if os.environ.get("VOYAGE_API_KEY") and os.environ.get("VOYAGE_AI_MODEL"):
-        return VoyageEmbeddings(model=os.environ["VOYAGE_AI_MODEL"])
-    return OpenAIEmbeddings(chunk_size=200)
-
-
-def get_retriever() -> BaseRetriever:
-    weaviate_client = weaviate.Client(
-        url=WEAVIATE_URL,
-        auth_client_secret=weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY),
-    )
-    weaviate_client = Weaviate(
-        client=weaviate_client,
-        index_name=WEAVIATE_DOCS_INDEX_NAME,
-        text_key="text",
-        embedding=get_embeddings_model(),
-        by_text=False,
-        attributes=["source", "title"],
-    )
-    return weaviate_client.as_retriever(search_kwargs=dict(k=6))
-
-
-def create_retriever_chain(
-    llm: BaseLanguageModel, retriever: BaseRetriever
-) -> Runnable:
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(REPHRASE_TEMPLATE)
-    condense_question_chain = (
-        CONDENSE_QUESTION_PROMPT | llm | StrOutputParser()
-    ).with_config(
-        run_name="CondenseQuestion",
-    )
-    conversation_chain = condense_question_chain | retriever
-    return RunnableBranch(
-        (
-            RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
-                run_name="HasChatHistoryCheck"
-            ),
-            conversation_chain.with_config(run_name="RetrievalChainWithHistory"),
-        ),
-        (
-            RunnableLambda(itemgetter("question")).with_config(
-                run_name="Itemgetter:question"
-            )
-            | retriever
-        ).with_config(run_name="RetrievalChainWithNoHistory"),
-    ).with_config(run_name="RouteDependingOnChatHistory")
-
-
-def format_docs(docs: Sequence[Document]) -> str:
-    formatted_docs = []
-    for i, doc in enumerate(docs):
-        doc_string = f"<doc id='{i}'>{doc.page_content}</doc>"
-        formatted_docs.append(doc_string)
-    return "\n".join(formatted_docs)
-
-
-def serialize_history(request: ChatRequest):
-    chat_history = request["chat_history"] or []
-    converted_chat_history = []
-    for message in chat_history:
-        if message.get("human") is not None:
-            converted_chat_history.append(HumanMessage(content=message["human"]))
-        if message.get("ai") is not None:
-            converted_chat_history.append(AIMessage(content=message["ai"]))
-    return converted_chat_history
-
-
-def create_chain(
-    llm: BaseLanguageModel,
-    retriever: BaseRetriever,
-) -> Runnable:
-    retriever_chain = create_retriever_chain(
-        llm,
-        retriever,
-    ).with_config(run_name="FindDocs")
-    _context = RunnableMap(
-        {
-            "context": retriever_chain | format_docs,
-            "question": itemgetter("question"),
-            "chat_history": itemgetter("chat_history"),
-        }
-    ).with_config(run_name="RetrieveDocs")
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", RESPONSE_TEMPLATE),
-            MessagesPlaceholder(variable_name="chat_history"),
-            ("human", "{question}"),
-        ]
-    )
-
-    response_synthesizer = (prompt | llm | StrOutputParser()).with_config(
-        run_name="GenerateResponse",
-    )
-    return (
-        {
-            "question": RunnableLambda(itemgetter("question")).with_config(
-                run_name="Itemgetter:question"
-            ),
-            "chat_history": RunnableLambda(serialize_history).with_config(
-                run_name="SerializeHistory"
-            ),
-        }
-        | _context
-        | response_synthesizer
-    )
-
-
-llm = ChatOpenAI(
-    model="gpt-4-1106-preview",
-    streaming=True,
-    temperature=0,
-)
-retriever = get_retriever()
-answer_chain = create_chain(
-    llm,
-    retriever,
-)
-
+answer_chain = create_answer_chain()
 
 add_routes(app, answer_chain, path="/chat", input_type=ChatRequest)
-
-
-class SendFeedbackBody(BaseModel):
-    run_id: UUID
-    key: str = "user_score"
-
-    score: Union[float, int, bool, None] = None
-    feedback_id: Optional[UUID] = None
-    comment: Optional[str] = None
 
 
 @app.post("/feedback")
@@ -267,12 +108,6 @@ async def send_feedback(body: SendFeedbackBody):
         feedback_id=body.feedback_id,
     )
     return {"result": "posted feedback successfully", "code": 200}
-
-
-class UpdateFeedbackBody(BaseModel):
-    feedback_id: UUID
-    score: Union[float, int, bool, None] = None
-    comment: Optional[str] = None
 
 
 @app.patch("/feedback")
@@ -307,10 +142,6 @@ async def aget_trace_url(run_id: str) -> str:
     if await _arun(client.run_is_shared, run_id):
         return await _arun(client.read_run_shared_link, run_id)
     return await _arun(client.share_run, run_id)
-
-
-class GetTraceBody(BaseModel):
-    run_id: UUID
 
 
 @app.post("/get_trace")
