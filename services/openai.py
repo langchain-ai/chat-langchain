37995068259e91090256aa038b1b77919bbdb78d
duob-ai <141818514+duob-ai@users.noreply.github.com>
2023-12-09 12:43:43 +0100

    Update openai.py

--- services/openai.py
+++ services/openai.py
@@ -20,6 +20,7 @@ def get_embeddings(texts: List[str]) -> List[List[float]]:
         Exception: If the OpenAI API call fails.
     """
     # Call the OpenAI API to get the embeddings
+    openai.api_type = "azure"
     deployment = "text-embedding-ada-002"
 
     try:
@@ -47,41 +48,3 @@ def get_embeddings(texts: List[str]) -> List[List[float]]:
         raise
 
 
-@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(3))
-def get_chat_completion(
-    messages,
-    model="gpt-3.5-turbo",  # use "gpt-4" for better results
-    deployment_id = None
-):
-    """
-    Generate a chat completion using OpenAI's chat completion API.
-
-    Args:
-        messages: The list of messages in the chat history.
-        model: The name of the model to use for the completion. Default is gpt-3.5-turbo, which is a fast, cheap and versatile model. Use gpt-4 for higher quality but slower results.
-
-    Returns:
-        A string containing the chat completion.
-
-    Raises:
-        Exception: If the OpenAI API call fails.
-    """
-    # call the OpenAI chat completion API with the given messages
-    # Note: Azure Open AI requires deployment id
-    response = {}
-    if deployment_id == None:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-        )
-    else:
-        response = openai.ChatCompletion.create(
-            deployment_id = deployment_id,
-            messages=messages,
-        )
-    
-
-    choices = response["choices"]  # type: ignore
-    completion = choices[0].message.content.strip()
-    print(f"Completion: {completion}")
-    return completion
