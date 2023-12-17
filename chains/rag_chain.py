a537a7930f91f529817887dd808f486bba554d73
duob-ai <141818514+duob-ai@users.noreply.github.com>
2023-12-17 22:56:11 +0100

    Update rag_chain.py

--- chains/rag_chain.py
+++ chains/rag_chain.py
@@ -100,7 +100,10 @@ def get_retriever() -> BaseRetriever:
         by_text=False,
         attributes=["source", "title", "score"],
     )
-    return weaviate_client.as_retriever(search_kwargs=dict(k=6, score=True))
+    return weaviate_client..as_retriever(
+                search_type="similarity_score_threshold",
+                search_kwargs={'score_threshold': 0.7}
+            )
 
 
 def create_retriever_chain(
