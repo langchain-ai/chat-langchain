5821e162f18549a217a14f39b79b3b43db228185
jacoblee93 <jacoblee93@gmail.com>
2024-01-19 11:34:08 -0800

    Updates frontend to use RemoteRunnable, sanitize returned HTML

--- chat-langchain/app/components/ChatWindow.tsx
+++ chat-langchain/app/components/ChatWindow.tsx
@@ -2,6 +2,9 @@
 
 import React, { useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { RemoteRunnable } from "langchain/runnables/remote";
+import { applyPatch } from "@langchain/core/utils/json_patch";
+
 import { EmptyState } from "../components/EmptyState";
 import { ChatMessageBubble, Message } from "../components/ChatMessageBubble";
 import { AutoResizeTextarea } from "./AutoResizeTextarea";
@@ -9,9 +12,6 @@ import { marked } from "marked";
 import { Renderer } from "marked";
 import hljs from "highlight.js";
 import "highlight.js/styles/gradient-dark.css";
-
-import { fetchEventSource } from "@microsoft/fetch-event-source";
-import { applyPatch } from "fast-json-patch";
 
 import "react-toastify/dist/ReactToastify.css";
 import {
@@ -87,87 +87,75 @@ export function ChatWindow(props: {
     try {
       const sourceStepName = "FindDocs";
       let streamedResponse: Record<string, any> = {};
-      await fetchEventSource(apiBaseUrl + "/chat/stream_log", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "text/event-stream",
-        },
-        body: JSON.stringify({
-          input: {
-            question: messageValue,
-            chat_history: chatHistory,
-          },
-          config: {
-            metadata: {
-              conversation_id: conversationId,
-            },
-          },
-          include_names: [sourceStepName],
-        }),
-        openWhenHidden: true,
-        onerror(err) {
-          throw err;
-        },
-        onmessage(msg) {
-          if (msg.event === "end") {
-            setChatHistory((prevChatHistory) => [
-              ...prevChatHistory,
-              { human: messageValue, ai: accumulatedMessage },
-            ]);
-            setIsLoading(false);
-            return;
-          }
-          if (msg.event === "data" && msg.data) {
-            const chunk = JSON.parse(msg.data);
-            streamedResponse = applyPatch(
-              streamedResponse,
-              chunk.ops,
-            ).newDocument;
-            if (
-              Array.isArray(
-                streamedResponse?.logs?.[sourceStepName]?.final_output?.output,
-              )
-            ) {
-              sources = streamedResponse.logs[
-                sourceStepName
-              ].final_output.output.map((doc: Record<string, any>) => ({
-                url: doc.metadata.source,
-                title: doc.metadata.title,
-              }));
-            }
-            if (streamedResponse.id !== undefined) {
-              runId = streamedResponse.id;
-            }
-            if (Array.isArray(streamedResponse?.streamed_output)) {
-              accumulatedMessage = streamedResponse.streamed_output.join("");
-            }
-            const parsedResult = marked.parse(accumulatedMessage);
-
-            setMessages((prevMessages) => {
-              let newMessages = [...prevMessages];
-              if (
-                messageIndex === null ||
-                newMessages[messageIndex] === undefined
-              ) {
-                messageIndex = newMessages.length;
-                newMessages.push({
-                  id: Math.random().toString(),
-                  content: parsedResult.trim(),
-                  runId: runId,
-                  sources: sources,
-                  role: "assistant",
-                });
-              } else if (newMessages[messageIndex] !== undefined) {
-                newMessages[messageIndex].content = parsedResult.trim();
-                newMessages[messageIndex].runId = runId;
-                newMessages[messageIndex].sources = sources;
-              }
-              return newMessages;
-            });
-          }
+      const remoteChain = new RemoteRunnable({
+        url: apiBaseUrl + "/chat",
+        options: {
+          timeout: 60000,
         },
       });
+      const streamLog = await remoteChain.streamLog(
+        {
+          question: messageValue,
+          chat_history: chatHistory,
+        },
+        {
+          metadata: {
+            conversation_id: conversationId,
+          },
+        },
+        {
+          includeNames: [sourceStepName],
+        },
+      );
+      for await (const chunk of streamLog) {
+        streamedResponse = applyPatch(streamedResponse, chunk.ops).newDocument;
+        if (
+          Array.isArray(
+            streamedResponse?.logs?.[sourceStepName]?.final_output?.output,
+          )
+        ) {
+          sources = streamedResponse.logs[
+            sourceStepName
+          ].final_output.output.map((doc: Record<string, any>) => ({
+            url: doc.metadata.source,
+            title: doc.metadata.title,
+          }));
+        }
+        if (streamedResponse.id !== undefined) {
+          runId = streamedResponse.id;
+        }
+        if (Array.isArray(streamedResponse?.streamed_output)) {
+          accumulatedMessage = streamedResponse.streamed_output.join("");
+        }
+        const parsedResult = marked.parse(accumulatedMessage);
+
+        setMessages((prevMessages) => {
+          let newMessages = [...prevMessages];
+          if (
+            messageIndex === null ||
+            newMessages[messageIndex] === undefined
+          ) {
+            messageIndex = newMessages.length;
+            newMessages.push({
+              id: Math.random().toString(),
+              content: parsedResult.trim(),
+              runId: runId,
+              sources: sources,
+              role: "assistant",
+            });
+          } else if (newMessages[messageIndex] !== undefined) {
+            newMessages[messageIndex].content = parsedResult.trim();
+            newMessages[messageIndex].runId = runId;
+            newMessages[messageIndex].sources = sources;
+          }
+          return newMessages;
+        });
+      }
+      setChatHistory((prevChatHistory) => [
+        ...prevChatHistory,
+        { human: messageValue, ai: accumulatedMessage },
+      ]);
+      setIsLoading(false);
     } catch (e) {
       setMessages((prevMessages) => prevMessages.slice(0, -1));
       setIsLoading(false);
