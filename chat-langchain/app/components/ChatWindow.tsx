37a1bdca2dcd29a98517f4fef61c4b9cfc961106
jacoblee93 <jacoblee93@gmail.com>
2024-01-24 16:51:27 -0800

    Readd Mixtral via Fireworks

--- chat-langchain/app/components/ChatWindow.tsx
+++ chat-langchain/app/components/ChatWindow.tsx
@@ -237,10 +237,10 @@ export function ChatWindow(props: { conversationId: string }) {
             >
               <option value="openai_gpt_3_5_turbo">GPT-3.5-Turbo</option>
               <option value="anthropic_claude_2_1">Claude-2.1</option>
-              {/* <option value="fireworks_mixtral">
-                Mixtral (via Fireworks.ai)
-              </option> */}
               <option value="google_gemini_pro">Google Gemini Pro</option>
+              <option value="fireworks_mixtral">
+                Mixtral (via Fireworks.ai)
+              </option>
             </Select>
           </div>
         </div>
