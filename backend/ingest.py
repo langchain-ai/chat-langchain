72ddc4c967bc5284d94851456b5971b2fb792978
Prabha Arivalagan <prabhakaran.mails@gmail.com>
2024-05-15 18:10:09 -0500

    removed unused lib

--- backend/ingest.py
+++ backend/ingest.py
@@ -2,12 +2,11 @@
 import logging
 import os
 import re
-from parser import langchain_docs_extractor
 
 import weaviate
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 from constants import WEAVIATE_DOCS_INDEX_NAME
-from langchain.document_loaders import RecursiveUrlLoader, SitemapLoader
+from langchain.document_loaders import RecursiveUrlLoader
 from langchain.indexes import SQLRecordManager, index
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.utils.html import PREFIXES_TO_IGNORE_REGEX, SUFFIXES_TO_IGNORE_REGEX
