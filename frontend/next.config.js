4177149c9cbbdd0e70868599c304e37830688443
Prabha Arivalagan <prabhakaran.mails@gmail.com>
2024-05-21 00:22:21 -0500

    fixing errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Add other necessary configurations here
+};
 
 module.exports = nextConfig;
