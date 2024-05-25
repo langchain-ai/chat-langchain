7f03cd621e6f7133f508cb74ee56ba0811edec5e
Katerina Malakhova <katerina.malakhova@cropguard.ai>
2024-05-25 18:33:45 -0400

    add google-analytics script

--- chat-langchain/app/layout.tsx
+++ chat-langchain/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from 'next/script'; // Import the Script component for Google Analytics
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
+            {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-DRFPTM9QR0"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DRFPTM9QR0', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      {/* End of Google Analytics */}
       <body className={`${inter.className} h-full`}>
         <div
           className="flex flex-col h-full md:p-8"
