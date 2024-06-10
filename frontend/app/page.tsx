b423b0aafcade1ee39f6195fbf8374ceb31e1f69
Anthony Hai <namsaoquan@gmail.com>
2024-06-10 16:39:10 +0700

    feat: add color mode to charka ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ToastContainer } from "react-toastify";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript  } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Client } from "@langchain/langgraph-sdk";
 
@@ -9,13 +9,16 @@ import { ChatWindow } from "./components/ChatWindow";
 import { LangGraphClientContext } from "./hooks/useLangGraphClient";
 import { apiBaseUrl } from "./utils/constants";
 
+import theme from './theme'
+
 export default function Home() {
   const queryClient = new QueryClient();
   const langGraphClient = new Client({ apiUrl: apiBaseUrl });
   return (
     <LangGraphClientContext.Provider value={langGraphClient}>
       <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ChakraProvider theme={theme}>
           <ToastContainer />
           <ChatWindow />
         </ChakraProvider>
