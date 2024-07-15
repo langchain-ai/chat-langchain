cb761d9df6bdf3e738804551073d04a71f24fb87
vbarda <vadym@langchain.dev>
2024-07-15 17:22:04 -0400

    nit

--- backend/ingest.py
+++ backend/ingest.py
@@ -104,10 +104,8 @@ def ingest_docs():
     embedding = get_embeddings_model()
 
     client = weaviate.connect_to_wcs(
-        cluster_url=os.environ["WEAVIATE_URL"],
-        auth_credentials=weaviate.classes.init.Auth.api_key(
-            os.environ.get("WEAVIATE_API_KEY", "not_provided")
-        ),
+        cluster_url=WEAVIATE_URL,
+        auth_credentials=weaviate.classes.init.Auth.api_key(WEAVIATE_API_KEY),
         skip_init_checks=True,
     )
     vectorstore = WeaviateVectorStore(
