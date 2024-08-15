606db0bf2d8fb2c74b9c6b12b669e63b226f2ba9
vbarda <vadym@langchain.dev>
2024-08-15 14:41:24 -0400

    fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,8 +8,8 @@ import { Client } from "@langchain/langgraph-sdk";
 import { ChatWindow } from "./components/ChatWindow";
 import { LangGraphClientContext } from "./hooks/useLangGraphClient";
 
-const apiUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
+  ? process.env.NEXT_PUBLIC_API_URL
   : "http://localhost:3000/api/";
 
 export default function Home() {
