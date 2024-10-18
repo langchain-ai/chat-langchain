d37c42ab8e17e06c6352eeab0e4d46dd551ac59b
David Xu <thexudavid@gmail.com>
2024-10-18 14:56:21 -0700

    chore: better names for langsmith prompts

--- backend/retrieval_graph/prompts.py
+++ backend/retrieval_graph/prompts.py
@@ -3,12 +3,12 @@ from langchain import hub
 """Default prompts."""
 
 # fetch from langsmith
-router_system_prompt = hub.pull("dxu-router-prompt").messages[0].prompt.template
-general_system_prompt = hub.pull("dxu-system-prompt").messages[0].prompt.template
-more_info_system_prompt = hub.pull("dxu-more-info-prompt").messages[0].prompt.template
-research_plan_system_prompt = hub.pull("dxu-research-plan-prompt").messages[0].prompt.template
-response_system_prompt = hub.pull("dxu-response-prompt").messages[0].prompt.template
-generate_queries_system_prompt = hub.pull("dxu-generate-queries-prompt").messages[0].prompt.template
+router_system_prompt = hub.pull("chat-langchain-router-prompt").messages[0].prompt.template
+generate_queries_system_prompt = hub.pull("chat-langchain-generate-queries-prompt").messages[0].prompt.template
+more_info_system_prompt = hub.pull("chat-langchain-more-info-prompt").messages[0].prompt.template
+research_plan_system_prompt = hub.pull("chat-langchain-research-plan-prompt").messages[0].prompt.template
+general_system_prompt = hub.pull("chat-langchain-general-prompt").messages[0].prompt.template
+response_system_prompt = hub.pull("chat-langchain-response-prompt").messages[0].prompt.template
 
 ROUTER_SYSTEM_PROMPT = router_system_prompt
 
