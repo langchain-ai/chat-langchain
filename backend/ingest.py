6a463aa0c212cf7a61665f851c5d2cd68bbf4deb
gintaras <gintaras.jakim@gmail.com>
2024-10-30 12:05:08 +0200

    ingest pdf document; fixed to work with ingestion

--- backend/ingest.py
+++ backend/ingest.py
@@ -2,18 +2,20 @@
 import logging
 import os
 import re
-from parser import langchain_docs_extractor
 
+from parser import langchain_docs_extractor
 import weaviate
 from bs4 import BeautifulSoup, SoupStrainer
 from constants import WEAVIATE_DOCS_INDEX_NAME
-from langchain.document_loaders import RecursiveUrlLoader, SitemapLoader
+# from langchain.document_loaders import RecursiveUrlLoader, SitemapLoader
+from langchain_community.document_loaders import PyPDFLoader, SitemapLoader, RecursiveUrlLoader
 from langchain.indexes import SQLRecordManager, index
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.utils.html import PREFIXES_TO_IGNORE_REGEX, SUFFIXES_TO_IGNORE_REGEX
-from langchain_community.vectorstores import Weaviate
+# from langchain_community.vectorstores import Weaviate
 from langchain_core.embeddings import Embeddings
 from langchain_openai import OpenAIEmbeddings
+from langchain_weaviate import WeaviateVectorStore
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -34,6 +36,31 @@ def metadata_extractor(meta: dict, soup: BeautifulSoup) -> dict:
         "language": html.get("lang", "") if html else "",
         **meta,
     }
+
+
+def load_pdf_docs():
+    file_path = "assets/05.Canada Water/CWA10-FMD-XXX-XX-SP-SP-000001.pdf"
+    folder_path = "assets"
+
+    if os.path.isdir(folder_path):
+        print("Folder exists.")
+    else:
+        print("Folder does not exist.")
+
+    if os.path.exists(file_path):
+        print("File exists.")
+    else:
+        print("File does not exist.")
+
+    loader = PyPDFLoader(file_path)
+    pages = loader.load()
+    # pages = []
+    # async for page in loader.alazy_load():
+    #     pages.append(page)
+
+    print(f"{pages[0].metadata}\n")
+    print(pages[0].page_content)
+    return pages
 
 
 def load_langchain_docs():
@@ -99,63 +126,130 @@ def ingest_docs():
     WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
     RECORD_MANAGER_DB_URL = os.environ["RECORD_MANAGER_DB_URL"]
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=4000, chunk_overlap=200)
     embedding = get_embeddings_model()
 
-    client = weaviate.Client(
-        url=WEAVIATE_URL,
-        auth_client_secret=weaviate.AuthApiKey(api_key=WEAVIATE_API_KEY),
-    )
-    vectorstore = Weaviate(
-        client=client,
-        index_name=WEAVIATE_DOCS_INDEX_NAME,
-        text_key="text",
-        embedding=embedding,
-        by_text=False,
-        attributes=["source", "title"],
-    )
+    with weaviate.connect_to_weaviate_cloud(
+        cluster_url=WEAVIATE_URL,
+        auth_credentials=weaviate.classes.init.Auth.api_key(WEAVIATE_API_KEY),
+        skip_init_checks=True,
+    ) as weaviate_client:
+        vectorstore = WeaviateVectorStore(
+            client=weaviate_client,
+            index_name=WEAVIATE_DOCS_INDEX_NAME,
+            text_key="text",
+            embedding=embedding,
+            attributes=["source", "title"],
+        )
+        record_manager = SQLRecordManager(
+            f"weaviate/{WEAVIATE_DOCS_INDEX_NAME}", db_url=RECORD_MANAGER_DB_URL
+        )
+        record_manager.create_schema()
 
-    record_manager = SQLRecordManager(
-        f"weaviate/{WEAVIATE_DOCS_INDEX_NAME}", db_url=RECORD_MANAGER_DB_URL
-    )
-    record_manager.create_schema()
+        docs = load_pdf_docs()
+        logger.info(
+            f"Loaded {len(docs)} docs from pdfs")
 
-    docs_from_documentation = load_langchain_docs()
-    logger.info(f"Loaded {len(docs_from_documentation)} docs from documentation")
-    docs_from_api = load_api_docs()
-    logger.info(f"Loaded {len(docs_from_api)} docs from API")
-    docs_from_langsmith = load_langsmith_docs()
-    logger.info(f"Loaded {len(docs_from_langsmith)} docs from Langsmith")
+        docs_transformed = text_splitter.split_documents(docs)
+        docs_transformed = [
+            doc for doc in docs_transformed if len(doc.page_content) > 10]
 
-    docs_transformed = text_splitter.split_documents(
-        docs_from_documentation + docs_from_api + docs_from_langsmith
-    )
-    docs_transformed = [doc for doc in docs_transformed if len(doc.page_content) > 10]
+        # We try to return 'source' and 'title' metadata when querying vector store and
+        # Weaviate will error at query time if one of the attributes is missing from a
+        # retrieved document.
+        for doc in docs_transformed:
+            if "source" not in doc.metadata:
+                doc.metadata["source"] = ""
+            if "title" not in doc.metadata:
+                doc.metadata["title"] = ""
 
-    # We try to return 'source' and 'title' metadata when querying vector store and
-    # Weaviate will error at query time if one of the attributes is missing from a
-    # retrieved document.
-    for doc in docs_transformed:
-        if "source" not in doc.metadata:
-            doc.metadata["source"] = ""
-        if "title" not in doc.metadata:
-            doc.metadata["title"] = ""
+        indexing_stats = index(
+            docs_transformed,
+            record_manager,
+            vectorstore,
+            cleanup="full",
+            source_id_key="source",
+            force_update=(os.environ.get("FORCE_UPDATE")
+                          or "false").lower() == "true",
+        )
 
-    indexing_stats = index(
-        docs_transformed,
-        record_manager,
-        vectorstore,
-        cleanup="full",
-        source_id_key="source",
-        force_update=(os.environ.get("FORCE_UPDATE") or "false").lower() == "true",
-    )
+        logger.info(f"Indexing stats: {indexing_stats}")
+        num_vecs = (
+            weaviate_client.collections.get(WEAVIATE_DOCS_INDEX_NAME)
+            .aggregate.over_all()
+            .total_count
+        )
+        logger.info(
+            f"LangChain now has this many vectors: {num_vecs}",
+        )
 
-    logger.info(f"Indexing stats: {indexing_stats}")
-    num_vecs = client.query.aggregate(WEAVIATE_DOCS_INDEX_NAME).with_meta_count().do()
-    logger.info(
-        f"LangChain now has this many vectors: {num_vecs}",
-    )
+
+def ingest_docs_new():
+    WEAVIATE_URL = os.environ["WEAVIATE_URL"]
+    WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
+    RECORD_MANAGER_DB_URL = os.environ["RECORD_MANAGER_DB_URL"]
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=4000, chunk_overlap=200)
+    embedding = get_embeddings_model()
+
+    with weaviate.connect_to_weaviate_cloud(
+        cluster_url=WEAVIATE_URL,
+        auth_credentials=weaviate.classes.init.Auth.api_key(WEAVIATE_API_KEY),
+        skip_init_checks=True,
+    ) as weaviate_client:
+        vectorstore = WeaviateVectorStore(
+            client=weaviate_client,
+            index_name=WEAVIATE_DOCS_INDEX_NAME,
+            text_key="text",
+            embedding=embedding,
+            attributes=["source", "title"],
+        )
+
+        record_manager = SQLRecordManager(
+            f"weaviate/{WEAVIATE_DOCS_INDEX_NAME}", db_url=RECORD_MANAGER_DB_URL
+        )
+        record_manager.create_schema()
+
+        docs = load_pdf_docs()
+        logger.info(
+            f"Loaded {len(docs)} docs from pdfs")
+
+        docs_transformed = text_splitter.split_documents(docs)
+        docs_transformed = [
+            doc for doc in docs_transformed if len(doc.page_content) > 10
+        ]
+
+        # We try to return 'source' and 'title' metadata when querying vector store and
+        # Weaviate will error at query time if one of the attributes is missing from a
+        # retrieved document.
+        for doc in docs_transformed:
+            if "source" not in doc.metadata:
+                doc.metadata["source"] = ""
+            if "title" not in doc.metadata:
+                doc.metadata["title"] = ""
+
+        indexing_stats = index(
+            docs_transformed,
+            record_manager,
+            vectorstore,
+            cleanup="full",
+            source_id_key="source",
+            force_update=(os.environ.get("FORCE_UPDATE")
+                          or "false").lower() == "true",
+        )
+
+        logger.info(f"Indexing stats: {indexing_stats}")
+        num_vecs = (
+            weaviate_client.collections.get(WEAVIATE_DOCS_INDEX_NAME)
+            .aggregate.over_all()
+            .total_count
+        )
+        logger.info(
+            f"LangChain now has this many vectors: {num_vecs}",
+        )
 
 
 if __name__ == "__main__":
-    ingest_docs()
+    ingest_docs_new()
