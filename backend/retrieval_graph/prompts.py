de3aca4de3165d43acf0c7f7c9c74c9160f40efb
vbarda <vadym@langchain.dev>
2024-11-04 09:43:12 -0500

    lint

--- backend/retrieval_graph/prompts.py
+++ backend/retrieval_graph/prompts.py
@@ -7,13 +7,17 @@ ROUTER_SYSTEM_PROMPT = (
     hub.pull("langchain-ai/chat-langchain-router-prompt").messages[0].prompt.template
 )
 GENERATE_QUERIES_SYSTEM_PROMPT = (
-    hub.pull("langchain-ai/chat-langchain-generate-queries-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-generate-queries-prompt")
+    .messages[0]
+    .prompt.template
 )
 MORE_INFO_SYSTEM_PROMPT = (
     hub.pull("langchain-ai/chat-langchain-more-info-prompt").messages[0].prompt.template
 )
 RESEARCH_PLAN_SYSTEM_PROMPT = (
-    hub.pull("langchain-ai/chat-langchain-research-plan-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-research-plan-prompt")
+    .messages[0]
+    .prompt.template
 )
 GENERAL_SYSTEM_PROMPT = (
     hub.pull("langchain-ai/chat-langchain-general-prompt").messages[0].prompt.template
