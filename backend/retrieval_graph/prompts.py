db276c96aa1207e5c0fe9791b5b41efd870bdf69
Vadym Barda <vadim.barda@gmail.com>
2024-11-04 09:44:25 -0500

    backend: make prompts public (#399)
    * backend: make prompts public
    
    * lint

--- backend/retrieval_graph/prompts.py
+++ backend/retrieval_graph/prompts.py
@@ -4,20 +4,24 @@ from langchain import hub
 
 # fetch from langsmith
 ROUTER_SYSTEM_PROMPT = (
-    hub.pull("chat-langchain-router-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-router-prompt").messages[0].prompt.template
 )
 GENERATE_QUERIES_SYSTEM_PROMPT = (
-    hub.pull("chat-langchain-generate-queries-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-generate-queries-prompt")
+    .messages[0]
+    .prompt.template
 )
 MORE_INFO_SYSTEM_PROMPT = (
-    hub.pull("chat-langchain-more-info-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-more-info-prompt").messages[0].prompt.template
 )
 RESEARCH_PLAN_SYSTEM_PROMPT = (
-    hub.pull("chat-langchain-research-plan-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-research-plan-prompt")
+    .messages[0]
+    .prompt.template
 )
 GENERAL_SYSTEM_PROMPT = (
-    hub.pull("chat-langchain-general-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-general-prompt").messages[0].prompt.template
 )
 RESPONSE_SYSTEM_PROMPT = (
-    hub.pull("chat-langchain-response-prompt").messages[0].prompt.template
+    hub.pull("langchain-ai/chat-langchain-response-prompt").messages[0].prompt.template
 )
