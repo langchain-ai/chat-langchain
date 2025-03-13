aad1f1cca2c41a757d5335a3529444c44d58466d
bracesproul <braceasproul@gmail.com>
2025-03-13 14:34:03 -0700

    fix: Only create thrad id at request time

--- frontend/app/components/thread-history/index.tsx
+++ frontend/app/components/thread-history/index.tsx
@@ -1,29 +1,21 @@
 import { TooltipIconButton } from "../ui/assistant-ui/tooltip-icon-button";
 import { SquarePen, History } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
-import { useToast } from "../../hooks/use-toast";
 import { Skeleton } from "../ui/skeleton";
 import React from "react";
 import { useGraphContext } from "../../contexts/GraphContext";
 import { groupThreads } from "./utils";
 import { ThreadsList } from "./thread-list";
+import { useQueryState } from "nuqs";
 
 const LoadingThread = () => <Skeleton className="w-full h-8 bg-[#373737]" />;
 
 function ThreadHistoryComponent() {
-  const { toast } = useToast();
   const { threadsData, userData, graphData } = useGraphContext();
-  const {
-    userThreads,
-    isUserThreadsLoading,
-    threadId,
-    deleteThread,
-    createThread,
-    getUserThreads,
-  } = threadsData;
+  const { userThreads, isUserThreadsLoading, deleteThread } = threadsData;
   const { userId } = userData;
-  const { messages, switchSelectedThread, setMessages } = graphData;
-  const isEmpty = messages.length === 0;
+  const { switchSelectedThread, setMessages } = graphData;
+  const [_threadId, setThreadId] = useQueryState("threadId");
 
   const clearMessages = () => {
     setMessages([]);
@@ -40,25 +32,9 @@ function ThreadHistoryComponent() {
     deleteThreadAndClearMessages,
   );
 
-  const createAndSetupNewThread = async () => {
-    if (!userId) {
-      toast({
-        title: "Error creating thread",
-        description: "Your user ID was not found. Please try again later.",
-      });
-      return;
-    }
-    const currentThread = userThreads.find(
-      (thread) => thread.thread_id === threadId,
-    );
-    if (currentThread && !currentThread.values && isEmpty) {
-      return;
-    }
-
+  const createNewSession = async () => {
+    setThreadId(null);
     clearMessages();
-    await createThread(userId);
-    // Re-fetch threads so that the new thread shows up.
-    await getUserThreads(userId);
   };
 
   return (
@@ -73,7 +49,7 @@ function ThreadHistoryComponent() {
                 tooltip="New chat"
                 variant="ghost"
                 className="w-fit p-2"
-                onClick={createAndSetupNewThread}
+                onClick={createNewSession}
               >
                 <SquarePen className="w-5 h-5" />
               </TooltipIconButton>
@@ -121,7 +97,7 @@ function ThreadHistoryComponent() {
             tooltip="New chat"
             variant="ghost"
             className="w-fit h-fit p-2"
-            onClick={createAndSetupNewThread}
+            onClick={createNewSession}
           >
             <SquarePen className="w-6 h-6" />
           </TooltipIconButton>
