6639bd1d42b1e6253cf245932cd340c70b6970bb
augustCotton123 <wco@verafiles.org>
2025-04-22 15:27:20 +0800

    voyage 3 lite retrieval assessment

--- backend/constants.py
+++ backend/constants.py
@@ -1,3 +1,3 @@
-WEAVIATE_DOCS_INDEX_NAME = "wy_test_OpenAI_text_embedding_3_small"
-# WEAVIATE_DOCS_INDEX_NAME = "wy_test_VoyageAI_voyage_3_lite"
+# WEAVIATE_DOCS_INDEX_NAME = "wy_test_OpenAI_text_embedding_3_small"
+WEAVIATE_DOCS_INDEX_NAME = "wy_test_VoyageAI_voyage_3_lite"
 # WEAVIATE_DOCS_INDEX_NAME = "wy_test_VoyageAI_voyage_law_2"
