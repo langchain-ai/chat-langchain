be969648a636def60b827be91a03db7ac04778c3
vfzale <zcham@verafiles.org>
2025-05-09 18:23:08 +0800

    hotjar changes

--- frontend/app/hotjar_googleanalytics.tsx
+++ frontend/app/hotjar_googleanalytics.tsx
@@ -1,22 +1,32 @@
 "use client";
-
+import Script from "next/script";
 export const Hotjar_GoogleAnalytics_Snippet = () => {
     return (
         <head>
-            <script id="hotjar-snippet">
-            {`
-                (function(h,o,t,j,a,r){
-                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:6393685,hjsv:6};
-                    a=o.getElementsByTagName('head')[0];
-                    r=o.createElement('script');r.async=1;
-                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `}
-            </script>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-JN1K8T232N"></script>
-            <script>
+            <Script id="hotjar-snippet">
+                {`
+                    (function(h,o,t,j,a,r){
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:6393685,hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                `}
+                {/* {
+                    "(function(h,o,t,j,a,r){\n" +
+                    "h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};\n" +
+                    "h._hjSettings={hjid:6393685,hjsv:6};\n" +
+                    "a=o.getElementsByTagName('head')[0];\n" +
+                    "r=o.createElement('script');r.async=1;\n" +
+                    "r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;\n" +
+                    "a.appendChild(r);\n" +
+                    "})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
+                } */}
+            </Script>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JN1K8T232N"></Script>
+            <Script>
             {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
@@ -24,7 +34,7 @@ export const Hotjar_GoogleAnalytics_Snippet = () => {
 
                 gtag('config', 'G-JN1K8T232N');
             `}
-            </script>
+            </Script>
         </head>
     );
 };
