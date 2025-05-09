71761d67e1b77e7c07b37052fdb8cf6ace689527
vfzale <zcham@verafiles.org>
2025-05-09 18:46:51 +0800

    id attribute for script

--- frontend/app/hotjar_googleanalytics.tsx
+++ frontend/app/hotjar_googleanalytics.tsx
@@ -14,19 +14,9 @@ export const Hotjar_GoogleAnalytics_Snippet = () => {
                         a.appendChild(r);
                     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
                 `}
-                {/* {
-                    "(function(h,o,t,j,a,r){\n" +
-                    "h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};\n" +
-                    "h._hjSettings={hjid:6393685,hjsv:6};\n" +
-                    "a=o.getElementsByTagName('head')[0];\n" +
-                    "r=o.createElement('script');r.async=1;\n" +
-                    "r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;\n" +
-                    "a.appendChild(r);\n" +
-                    "})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
-                } */}
             </Script>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JN1K8T232N"></Script>
-            <Script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-JN1K8T232N"></script>
+            <Script id="googleanalytics-snippet">
             {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
