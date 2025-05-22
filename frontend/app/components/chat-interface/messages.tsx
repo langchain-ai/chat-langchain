5e0ea85c53ca3be01a08ca5efd965145a3645c42
vfzale <zcham@verafiles.org>
2025-05-22 19:12:15 +0800

    ui changes 8

--- frontend/app/components/chat-interface/messages.tsx
+++ frontend/app/components/chat-interface/messages.tsx
@@ -137,8 +137,9 @@ export const AssistantMessage: FC = () => {
         <MessagePrimitive.Content components={{ Text: MarkdownText }} />
         {shouldRenderMessageBreak ? (
           <hr className="relative left-1/2 -translate-x-1/2 w-[90vw] sm:w-[45vw] mt-4 sm:mt-6 border-gray-600" />
-        ) : <FeedbackButtons />}
+        ) : null}
         {/* {isLast && <FeedbackButtons />} */}
+        <FeedbackButtons />
       </div>
     </MessagePrimitive.Root>
   );
