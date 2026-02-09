126e29522c97696dd50086ed39ceff9408e34da6
liam-langchain <liam@langchain.dev>
2026-02-09 11:04:44 -0800

    Remove all files

--- Procfile
+++
@@ -1,2 +0,0 @@
-# Modify this Procfile to fit your needs
-web: uvicorn backend.main:app --host 0.0.0.0 --port 8080
--- backend/embeddings.py
+++
@@ -1,6 +0,0 @@
-from langchain_core.embeddings import Embeddings
-from langchain_openai import OpenAIEmbeddings
-
-
-def get_embeddings_model() -> Embeddings:
-    return OpenAIEmbeddings(model="text-embedding-3-small", chunk_size=200)
